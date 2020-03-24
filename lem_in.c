1cb4fec28007e664494b36a6c1743ea10eef3789
Lebus Nzimande <lnzimande@africanbank.net@DEV10WRK.AFRICANBANK.NET>
2020-03-24 07:24:50 +0200

    corona upon us

--- lem_in.c
+++ lem_in.c
@@ -20,6 +20,15 @@ typedef struct      _root {
     struct routes   *next;
 }                   root;
 
+void    print_array(char **arr)
+{
+    while (*arr)
+    {
+        ft_putendl(*arr);
+        arr++;
+    }
+}
+
 char    *getPosition(char **arr, char *position)
 {
     char     *pos_found;
@@ -78,9 +87,21 @@ void   getLinks(char **arr, links **head)
     }
 }
 
+void    getRooms(char **arr, char *start, char *end, links **rooms)
+{
+    while (*arr)
+    {
+        if (ft_strcmp(*arr, start) && ft_strcmp(*arr, end) && \
+        !ft_strchr(*arr, '#') && !ft_strchr(*arr, '-'))
+            storeLink(*arr, rooms);
+        arr++;
+    }
+}
+
 int     main(void)
 {
     links   *head;
+    links   *room;
     int     antsNumber;
     char    *start;
     char    *startName;
@@ -88,21 +109,37 @@ int     main(void)
     char    *endName;
 
     head = NULL;
+    room = NULL;
     char **name;
-    char *str = "3'##start'0 1 0'##end'1 5 0'2 9 0'3 13 0'0-2'2-3'3-1";
-    // char *str2 = "3'2 5 0'##start'0 1 2'##end'1 9 2'3 5 4'0-2'0-3'2-1'3-1'2-3";
+    // char *str = "3'##start'0 1 0'##end'1 5 0'2 9 0'3 13 0'0-2'2-3'3-1";
+    char *str2 = "0'2 5 0'##start'0 1 2'##end'1 9 2'3 5 4'0-2'0-3'2-1'3-1'2-3";
 
-    name = ft_strsplit(str, '\'');
-    antsNumber = getNumberOfAnts((*name)++);
+    name = ft_strsplit(str2, '\'');
+    antsNumber = getNumberOfAnts((*name));
     start = getPosition(name, START);
     startName = ft_strsub(start, 0, ft_strchr(start, ' ') - start);
     end = getPosition(name, END);
     endName = ft_strsub(end, 0, ft_strchr(end, ' ') - end);
     getLinks(name, &head);
+    getRooms(++name, start, end, &room);
+    // getRoutes(start, antsNumber);
+    ft_putendl("Ants Number");
+    ft_putnbr(antsNumber);
+    ft_putchar('\n');
+    ft_putendl("Start postion");
+    ft_putendl(start);
+    ft_putendl("End postion");
+    ft_putendl(end);
+    ft_putendl("links");
     while (head) {
         ft_putendl(head->data);
         head = head->next;
     }
-    
+    ft_putendl("rooms");
+    while (room)
+    {
+        ft_putendl(room->data);
+        room = room->next;
+    }
     return (0);
 }
